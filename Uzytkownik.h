298d089b54a45b3e721d1d461df5355afd07563b
Mateusz Stankowski <stankowski1992@gmail.com>
2020-06-14 15:50:09 +0200

    Add registered user to vector

--- Uzytkownik.h
+++ Uzytkownik.h
@@ -7,4 +7,15 @@ class Uzytkownik
     int id;
     string login;
     string haslo;
+
+public:
+    //settery
+    void ustawId(int noweId);
+    void ustawLogin(string nowyLogin);
+    void ustawHaslo(string noweHaslo);
+
+    //gettery
+    int pobierzId();
+    string pobierzLogin();
+    string pobierzHaslo();
 };
